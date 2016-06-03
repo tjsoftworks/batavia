09f2030fd4157d95cf92b028d6f957ea40076fe6
Katie McLaughlin <katie@glasnt.com>
2016-06-03 13:52:25 -0700

    Reduce complexity of traceback regex

--- tests/datatypes/test_str.py
+++ tests/datatypes/test_str.py
@@ -199,7 +199,6 @@ class BinaryStrOperationTests(BinaryOperationTestCase, TranspileTestCase):
         'test_modulo_set',
 
         'test_multiply_bytearray',
-        'test_multiply_bytes',
         'test_multiply_class',
         'test_multiply_complex',
         'test_multiply_frozenset',
@@ -345,7 +344,6 @@ class InplaceStrOperationTests(InplaceOperationTestCase, TranspileTestCase):
 
         'test_multiply_bool',
         'test_multiply_bytearray',
-        'test_multiply_bytes',
         'test_multiply_class',
         'test_multiply_complex',
         'test_multiply_dict',
