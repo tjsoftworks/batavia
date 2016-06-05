1c650971c85c8b659e239bee3542afc1fa365ce5
Terry J Fundak <terry@tjsoftworks.com>
2016-06-04 17:10:42 -0700

    Added lines 'lost' during rebase.

--- tests/builtins/test_abs.py
+++ tests/builtins/test_abs.py
@@ -4,6 +4,7 @@ from .. utils import TranspileTestCase, BuiltinFunctionTestCase
 
 
 class AbsTests(TranspileTestCase):
+    @expectedFailure
     def test_abs_not_implemented(self):
         self.assertCodeExecution("""
             class NotAbsLike:
@@ -17,6 +18,7 @@ class BuiltinAbsFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     functions = ["abs"]
 
     not_implemented = [
+        'test_bytes',
         'test_class',
         'test_complex',
     ]
